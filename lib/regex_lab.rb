2a653cf7abaf3ac0bc854652be20fe3016c94190
Ting Chung <tchung682@gmail.com>
2020-06-05 16:28:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.scan(/[aeiou]/)
+  if word.scan(/w\+[aeiou]/)
 end
 puts starts_with_a_vowel?("a advendure")
 def words_starting_with_un_and_ending_with_ing(text)
